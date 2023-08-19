38196b99d8a221fa72fc7ea8cbf19312e0ce729b
Matthew Wolfe <matthew.wolfe2@gmail.com>
2023-08-19 10:22:43 -0400

    move function out

--- src/components/JsonLiteral/JsonLiteral.tsx
+++ src/components/JsonLiteral/JsonLiteral.tsx
@@ -2,39 +2,45 @@ import { useContext, useMemo } from 'react';
 import { Flex, Text } from '@mantine/core';
 import { JsonViewContext } from 'context';
 
+import type { JsonLiteralValue } from 'types/json';
 import type { Props } from './JsonLiteral.types';
+
+const styles = {
+  root: {
+    alignItems: 'center',
+    gap: 8,
+    width: '100%',
+  },
+};
+
+function getTextValue(literal: JsonLiteralValue) {
+  switch (literal.type) {
+    case 'null':
+      return 'null';
+    case 'undefined':
+      return 'undefined';
+    case 'string':
+      return `"${literal.value}"`;
+    default:
+      return literal.value.toString();
+  }
+}
 
 function JsonLiteral({ literal }: Props) {
   const { clipboardComponent: ClipboardComponent } =
     useContext(JsonViewContext);
 
-  const value = useMemo(() => {
-    switch (literal.type) {
-      case 'null':
-        return 'null';
-      case 'undefined':
-        return 'undefined';
-      case 'string':
-        return `"${literal.value}"`;
-      default:
-        return literal.value.toString();
-    }
-  }, [literal.type, literal.value]);
+  const textValue = useMemo(() => getTextValue(literal), [literal]);
 
   return (
-    <Flex
-      sx={{
-        alignItems: 'center',
-        gap: 8,
-        width: '100%',
-      }}
-      px="xs"
-    >
+    <Flex sx={styles.root} px="xs">
       <Text fz="md">
-        "{literal.key}": {value}
+        "{literal.key}": {textValue}
       </Text>
 
-      {ClipboardComponent !== undefined && <ClipboardComponent value={value} />}
+      {ClipboardComponent !== undefined && (
+        <ClipboardComponent value={textValue} />
+      )}
     </Flex>
   );
 }
